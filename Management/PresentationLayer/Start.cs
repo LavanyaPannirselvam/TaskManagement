8aa5c8c4702484229b8874a35cfe172389c27f85
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-18 18:13:32 +0530

    Add files via upload

---
+++ Management/PresentationLayer/Start.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp1.Enumeration;
+using ConsoleApp1.Utils;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.PresentationLayer
+{
+    public class Start
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Welcome");
+            while (true)
+            {
+                Console.WriteLine("Choose your option:");
+                foreach(MainMenuOptions option in Enum.GetValues(typeof(MainMenuOptions)))
+                    Console.WriteLine((int)option+1+" . "+option.ToString());
+                int choice =Validator.getIntInRange(Enum.GetValues(typeof(MainMenuOptions)).Length);
+                MainMenuOptions options = (MainMenuOptions)choice - 1;
+                switch(choice)
+                {
+                    case MainMenuOptions.SIGNUP:
+                        break;
+                    case MainMenuOptions.SIGNIN:
+                        break;
+                    case MainMenuOptions.QUIT:Environment.Exit(0);
+                        break;
+                }
+            }
+        }
+    }
+}
+
