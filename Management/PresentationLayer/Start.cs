409f22bd0ee847a63609f44b166d55b38d912026
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-19 17:28:29 +0530

    Add files via upload

--- Management/PresentationLayer/Start.cs
+++ Management/PresentationLayer/Start.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleApp1.Enumeration;
+﻿using ConsoleApp1.DataBase;
+using ConsoleApp1.Enumeration;
 using ConsoleApp1.Utils;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -10,6 +11,12 @@ namespace ConsoleApp1.PresentationLayer
 {
     public class Start
     {
+        private static DatabaseHandler handler;
+
+        public Start()
+        {
+            handler = new DatabaseHandler();
+        }
         public static void Run()
         {
             Console.WriteLine("Welcome");
@@ -18,9 +25,9 @@ namespace ConsoleApp1.PresentationLayer
                 Console.WriteLine("Choose your option:");
                 foreach(MainMenuOptions option in Enum.GetValues(typeof(MainMenuOptions)))
                     Console.WriteLine((int)option+1+" . "+option.ToString());
-                int choice =Validator.getIntInRange(Enum.GetValues(typeof(MainMenuOptions)).Length);
-                MainMenuOptions options = (MainMenuOptions)choice - 1;
-                switch(choice)
+                int choice = Utils.Validator.getIntInRange(Enum.GetValues(typeof(MainMenuOptions)).Length);
+                MainMenuOptions options = (MainMenuOptions)(choice - 1);
+                switch(options)
                 {
                     case MainMenuOptions.SIGNUP:
                         break;
