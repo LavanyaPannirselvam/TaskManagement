87d7b750c67b3ca1a0ae2b12c8f7f1f7bcddcec6
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-19 17:24:26 +0530

    Add files via upload

--- Management/Database/Database.cs
+++ Management/Database/Database.cs
@@ -10,54 +10,59 @@ namespace ConsoleApp1.DataBase
 {
     public class Database
     {
-        private sealed Dictionary<int, User> allUsers;
-        private sealed Dictionary<int, string> userCredentials;
-        private sealed Dictionary<int, Project> allProjects;
-        private sealed User currentUser;
+        private Dictionary<int, User> allUsers;
+        private Dictionary<int, string> userCredentials;
+        private Dictionary<int, Project> allProjects;
+        private User? currentUser = null;
         private Database() {
             allUsers = new Dictionary<int, User>();
             allProjects = new Dictionary<int, Project>();
             userCredentials = new Dictionary<int, string>();
         }
 
-        private static Database? instance = null;
+        private static readonly Database? instance = null;
         public static Database GetInstance() 
         {
             if(instance == null)
                 return new Database();
             return instance;
         }
-        
+        //Projects section
         public Result AddProject(Project project)
         {
-            if (allProjects.ContainsKey(project.ProjectId))
+            if (allProjects.ContainsKey(project.Id))
                 return Result.FAILURE;
-            allProjects.Add(project.ProjectId, project);
+            allProjects.Add(project.Id, project);
             return Result.SUCCESS;
         }
 
-        public string GetProject(int projectId)
+        public Project GetProject(int projectId)
         {
             if (allProjects.ContainsKey(projectId))
-                return allProjects[projectId].ToString();
-            else return "Project not available";
+                return allProjects[projectId];
+            else return null;
         }
 
         public Result DeleteProject(int projectId) 
         {
             if (allProjects.ContainsKey(projectId))
             {
-                allProjects.Remove(projectId);
-                return Result.SUCCESS;
+                if (GetProject(projectId).AssignedUsers.Count == 0)
+                {
+                    allProjects.Remove(projectId);
+                    return Result.SUCCESS;
+                }
+                else return Result.PARTIAL;
             }
             return Result.FAILURE;             
         }
-
-        public Result AddUser(User user)
+        //User section
+        public Result AddUser(User user,string password)
         {
             if(allUsers.ContainsKey(user.UserId))
                 return Result.FAILURE;
             allUsers.Add(user.UserId, user);
+            userCredentials.Add(user.UserId, password);
             return Result.SUCCESS;
         }
 
@@ -70,14 +75,37 @@ namespace ConsoleApp1.DataBase
 
         public Result DeleteUser()
         {
-            if (currentUser != null)
+            if(currentUser != null)
             {
-                allUsers.Remove(currentUser.UserId);
-                currentUser = null;
-                return Result.SUCCESS;
+                if (currentUser.AssignedProjects.Count == 0)
+                {
+                    allUsers.Remove(currentUser.UserId);
+                    userCredentials.Remove(currentUser.UserId);
+                    currentUser = null;
+                    return Result.SUCCESS;
+                }
+                else return Result.PARTIAL;
             }
             return Result.FAILURE;
         }
+
+        internal Result CheckUser(int userId,string password)
+        {
+            if (allUsers.ContainsKey(userId))
+            {
+                if (userCredentials[userId] == password)
+                    return Result.SUCCESS;
+                else return Result.PARTIAL;
+            }
+            else return Result.FAILURE;
+        }
+
+        /*internal string GetPassword(int userId)
+        {
+            if (userCredentials.ContainsKey(userId))
+                return userCredentials[userId];
+            else return null;
+        }*/
         public void SetCurrentUser(User user){ this.currentUser = user; }
 
         public User GetCurrentUser() { return currentUser;}
---
+++ Management/Database/DatabaseHandler.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleApp1.Enumeration;
+using ConsoleApp1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase
+{
+    public class DatabaseHandler
+    {
+        private Database database;
+        public DatabaseHandler()
+        {
+            database = Database.GetInstance();
+        }
+        public string SignUp(string password, string name, string email, Role role)
+        {
+            User newUser = new User(name, email, role);
+            if (database.AddUser(newUser,password) == Result.FAILURE)
+                return "User already exists";
+            else if (database.AddUser(newUser, password) != Result.SUCCESS) return "Some error occured";
+            else return "SignedUp successfully \nYour userId is : " + newUser.UserId;
+        }
+
+        public string LogIn(int userid, string password)
+        {
+            if (database.CheckUser(userid, password) == Result.SUCCESS)
+                return "Login successful";
+            else if (database.CheckUser(userid, password) == Result.PARTIAL)
+                return "Password incorrect";
+            else return "Invalid userId";
+                
+        }
+
+        public string LogOut() 
+        {
+            if (database.GetCurrentUser() != null)
+            {
+                database.SetCurrentUser(null);//TODO
+                return "LoggedOut successfully";
+            }
+            else return "Login yourself to proceed further";
+        }
+
+        public string SignOut()
+        {
+            if (database.DeleteUser() == Result.SUCCESS)
+                return "Your account deleted successfully";
+            else if (database.DeleteUser() == Result.PARTIAL) 
+                return "Cannot delete account now as you have projects pending to do";
+            else return "Login yourself to proceed further";
+        }
+    }
+}
