1623a629941408f3a46bf5d9a84104b0aa623342
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-20 16:14:14 +0530

    Add files via upload

--- Management/Enumeration/UserMenuOptions.cs
+++ Management/Enumeration/UserMenuOptions.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Enumeration
 {
-    public enum Role
+    public enum UserMenuOptions
     {
-        EMPLOYEE,MANAGER,LEAD,NONE
+        ASSIGN_USER,DEASSIGN_USER,VIEW_MY_PROJECTS,VIEW_PROJECT,CHANGE_PRIORITY,CHANGE_STATUS,CREATE_PROJECT,DELETE_PROJECT,LOGOUT,SIGNOUT
     }
 }
