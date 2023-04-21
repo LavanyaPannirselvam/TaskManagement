19f0bc0cf1969a97a337f0c7c17e2257e63876b8
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-21 17:43:27 +0530

    Add files via upload

---
+++ Management/Utils/ColorCode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Utils
+{
+    public class ColorCode
+    {
+        internal static void MenuCode()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        internal static void GetInputCode(string msg) 
+        {
+            Console.ForegroundColor= ConsoleColor.White;
+            Console.Write(msg + "\n");
+        }
+        internal static void SuccessCode(string msg) 
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.Write(msg + "\n");
+        }
+        internal static void FailureCode(string msg) 
+        {
+            Console.ForegroundColor= ConsoleColor.Red;
+            Console.Write(msg+"\n");
+        }
+        internal static void PartialCode(string msg) 
+        {
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+            Console.Write(msg+"\n");
+        }
+    }
+}
---
+++ Management/Utils/Validator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1.Utils
+{
+    public class Validator
+    {
+        public static int getIntInRange(int max)
+        {
+            int num;
+            if (int.TryParse(Console.ReadLine(), out num))
+            {
+                if (num > 0 && num <= max)
+                    return num;
+                else
+                {
+                    ColorCode.FailureCode("Wrong option entered");
+                    ColorCode.GetInputCode("Choose your choice : ");
+                    return getIntInRange(max);
+                }
+            }
+            else
+            {
+                ColorCode.FailureCode("Wrong format input");
+                ColorCode.GetInputCode("Enter the correct choice : ");
+                return getIntInRange(max);
+            }
+        }
+        public static bool ContainsSpecialOrNumericCharacters(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsLetter(c) && !char.IsWhiteSpace(c))
+                    return true;
+            }
+            return false;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+
+        public static bool IsValidPassword(string password)
+        {
+            string regex = "^(?=.*[a-z])(?=.+ *[A-Z])(?=.*\\d)+(?=.*[-+_!@#$%^&*., ?]).+$";
+            return Regex.IsMatch(password, regex, RegexOptions.IgnoreCase);
+        }
+    }
+}
+
