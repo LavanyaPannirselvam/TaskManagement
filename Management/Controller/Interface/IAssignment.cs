8da901524a27e8ba6a09ff77f92fa9ce0bcba70b
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-24 18:22:23 +0530

    Add files via upload

---
+++ Management/Controller/Interface/IAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApplication.Controller.Interface
+{
+    public interface IAssignment
+    {
+        string AssignUser(int projectId, int userId);
+        string DeassignUser(int projectId, int userId);
+    }
+}
+
---
+++ Management/Controller/Interface/IManage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApplication.Enumerations;
+
+namespace TaskManagementApplication.Controller.Interface
+{
+    public interface IManage
+    {
+        string Create(string name, string desc, StatusType status, PriorityType type, DateTime startDate, DateTime endDate);
+        string Remove(int id);
+        string View(int id);
+
+    }
+}
