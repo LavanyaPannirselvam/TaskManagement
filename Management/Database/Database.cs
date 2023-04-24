aba5120eef418b1c861649cc2a38816745ab413a
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-24 18:23:32 +0530

    Add files via upload

--- Management/Database/Database.cs
+++ Management/Database/Database.cs
@@ -1,40 +1,51 @@
-﻿using ConsoleApp1.Enumeration;
-using ConsoleApp1.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementApplication.Enumerations;
+using TaskManagementApplication.Model;
 
-namespace ConsoleApp1.DataBase
+namespace TaskManagementApplication.DataBase
+
 {
     public class Database
     {
-        private readonly Dictionary<int, User> _allUsers;
-        private readonly Dictionary<int, string> _userCredentials;
-        private readonly Dictionary<int, Project> _allProjects;
-
-        public static int CurrentUser { get; set; }      
+        private static Dictionary<int, User>? _allUsers;
+        private static Dictionary<int, string>? _userCredentials;
+        private static Dictionary<int, Project>? _allProjects;
+        private int currentUser;
+        public int CurrentUser { get { return currentUser; } set { currentUser = value; } }
         private Database()
         {
-            _allUsers = new Dictionary<int, User>();
-            _allProjects = new Dictionary<int, Project>();
-            _userCredentials = new Dictionary<int, string>();
+            _allUsers = new Dictionary<int, User>
+            {
+                {1,new User("Lavanya","lava@gmail.com",Role.EMPLOYEE) },
+                {2 ,new User("Prithivi","prithivi.8@gmail.com",Role.MANAGER) },
+                {3,new User("Deepika","deepi@gmail.com",Role.LEAD) }
+            };
+            _allProjects = new Dictionary<int, Project>
+            {
+                {1,new Project("P1","UI",2,StatusType.OPEN,PriorityType.MEDIUM,new DateTime(20/05/2023),new DateTime(20/06/2023)) },
+                {2,new Project("P2","Backend",2,StatusType.OPEN, PriorityType.LOW,new DateTime(01/06/2023),new DateTime(01/07/2023)) }
+            };
+            _userCredentials = new Dictionary<int, string>
+            {
+                {1,"123Q!a"},
+                {2,"234#Qas"},
+                {3,"Ik!2w34"}
+            };
         }
 
-        private static readonly Database? instance = null;
+        private static readonly Database instance = new();
         public static Database GetInstance()
         {
-            if (instance == null)
-                return new Database();
             return instance;
         }
         //Projects section
         public Result AddProject(Project project)
         {
-            if (_allProjects.ContainsKey(project.Id))
+            if (_allProjects!.ContainsKey(project.Id))
                 return Result.FAILURE;
             _allProjects.Add(project.Id, project);
             return Result.SUCCESS;
@@ -42,14 +53,14 @@ namespace ConsoleApp1.DataBase
 
         public Project GetProject(int projectId)
         {
-            if (_allProjects.ContainsKey(projectId))
+            if (_allProjects!.ContainsKey(projectId))
                 return _allProjects[projectId];
             else return null;
         }
 
         public Result DeleteProject(int projectId)
         {
-            if (_allProjects.ContainsKey(projectId))
+            if (_allProjects!.ContainsKey(projectId))
             {
                 if (GetProject(projectId).AssignedUsers.Count == 0)
                 {
@@ -63,20 +74,20 @@ namespace ConsoleApp1.DataBase
         //User section
         public Result AddUser(User user, string password)
         {
-            if (_allUsers.ContainsKey(user.UserId))
+            if (_allUsers!.ContainsKey(user.UserId))
                 return Result.FAILURE;
             _allUsers.Add(user.UserId, user);
-            _userCredentials.Add(user.UserId, password);
+            _userCredentials!.Add(user.UserId, password);
             return Result.SUCCESS;
         }
 
         public User GetUser(int userId)
         {
-             return _allUsers[userId];
+            return _allUsers![userId];
         }
         public bool IsUserAvailable(int userId)
         {
-            if(_allUsers.ContainsKey(userId))
+            if (_allUsers!.ContainsKey(userId))
                 return true;
             return false;
         }
@@ -86,8 +97,8 @@ namespace ConsoleApp1.DataBase
             {
                 if (GetUser(CurrentUser).AssignedProjects.Count == 0)
                 {
-                    _allUsers.Remove(CurrentUser);
-                    _userCredentials.Remove(CurrentUser);
+                    _allUsers!.Remove(CurrentUser);
+                    _userCredentials!.Remove(CurrentUser);
                     CurrentUser = 0;
                     return Result.SUCCESS;
                 }
@@ -96,14 +107,13 @@ namespace ConsoleApp1.DataBase
             else return Result.FAILURE;
         }
 
-        internal Result CheckUser(int userId, string password)
+        public Result CheckUser(int userId, string password)//pakka
         {
-            if (_allUsers.ContainsKey(userId))
+            if (_allUsers!.ContainsKey(userId))
             {
-                if (_userCredentials[userId] == password)
+                if (_userCredentials![userId] == password)
                 {
-                    CurrentUser=userId;
-                    //Console.WriteLine(CurrentUser);
+                    CurrentUser = userId;
                     return Result.SUCCESS;
                 }
                 else return Result.PARTIAL;
@@ -122,16 +132,5 @@ namespace ConsoleApp1.DataBase
                 return Result.FAILURE;
             }
         }
-        /*internal string GetPassword(int userId)
-        {
-            if (userCredentials.ContainsKey(userId))
-                return userCredentials[userId];
-            else return null;
-        }
-        public void SetCurrentUser(User user){ this.CurrentUser = user; }
-
-        public User GetCurrentUser() { return CurrentUser;}
-
-    }*/
     }
-}
+    }
--- Management/Database/DatabaseHandler.cs
+++ Management/Database/DatabaseHandler.cs
@@ -1,61 +1,57 @@
-﻿using ConsoleApp1.Enumeration;
-using ConsoleApp1.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagementApplication.DataBase;
+using TaskManagementApplication.Enumerations;
+using TaskManagementApplication.Model;
+using TaskManagementApplication.DataBase.Interface;
 
-namespace ConsoleApp1.DataBase
+namespace TaskManagementApplication.Controller
 {
-    public class DatabaseHandler : ISignProcess,ILogProcess
-    {
-        private Database _database = Database.GetInstance();
-        private static DatabaseHandler? _databaseHandler=null;
-        public static DatabaseHandler GetInstance() 
+        public class DatabaseHandler : ISignProcess, ILogProcess
         {
-            if(_databaseHandler==null)
-                return new DatabaseHandler();
-            return _databaseHandler;
-        }
-        public string SignUp(string name, string email, Role role, string password)
-        {
-            User newUser = new(name, email, role);
-            if (_database.AddUser(newUser,password) == Result.FAILURE)
-                return "User already exists";
-            //else if (database.AddUser(newUser, password) != Result.SUCCESS) 
-              //  return "Some error occured";
-            else return "SignedUp successfully \nYour userId is : " + newUser.UserId;
-        }
+            readonly Database _database = Database.GetInstance();
+            public string SignUp(string name, string email, Role role, string password)
+            {
+                User newUser = new(name, email, role);
+                if (_database.AddUser(newUser, password) == Result.FAILURE)
+                    return "User already exists";
+                //else if (database.AddUser(newUser, password) != Result.SUCCESS) 
+                //  return "Some error occured";
+                else return "SignedUp successfully \nYour userId is : " + newUser.UserId;
+            }
 
-        public string LogIn(int userid, string password)
-        {
-            if (_database.CheckUser(userid, password) == Result.SUCCESS)
-                return "Login successful";
-            else if (_database.CheckUser(userid, password) == Result.PARTIAL)
-                return "Password incorrect";
-            else return "Invalid userId";
-                
-        }
+            public string LogIn(int userid, string password)
+            {
+                if (_database.CheckUser(userid, password) == Result.SUCCESS)
+                    return "Login successful";
+                else if (_database.CheckUser(userid, password) == Result.PARTIAL)
+                    return "Password incorrect";
+                else return "Invalid userId";
 
-        public string LogOut() 
-        {
-            Console.WriteLine(Database.CurrentUser);
-            Result result = _database.LogOut();
-            if(result == Result.SUCCESS)
-                return "LoggedOut successfully";
-            else return "Logout unsuccessful";
-        }
+            }
 
-        public string SignOut()
-        {
-            Console.WriteLine(Database.CurrentUser);
-            Result result = _database.DeleteUser();
-            if (result == Result.SUCCESS)
-                return "Your account deleted successfully";
-            else if (result == Result.PARTIAL) 
-                return "Cannot delete account now as you have projects pending to do";
-            else return "Login yourself to proceed further";
+            public string LogOut()
+            {
+                //Console.WriteLine(Database.CurrentUser);
+                Result result = _database.LogOut();
+                if (result == Result.SUCCESS)
+                    return "LoggedOut successfully";
+                else return "Logout unsuccessful";
+            }
+
+            public string SignOut()
+            {
+                //Console.WriteLine(Database.CurrentUser);
+                Result result = _database.DeleteUser();
+                if (result == Result.SUCCESS)
+                    return "Your account deleted successfully";
+                else if (result == Result.PARTIAL)
+                    return "Cannot delete account now as you have projects pending to do";
+                else return "Login yourself to proceed further";
+            }
         }
     }
-}
