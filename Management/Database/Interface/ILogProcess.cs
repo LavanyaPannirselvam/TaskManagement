611d2f45843cdc0a94d23190d8a197075e6977f4
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-24 18:24:37 +0530

    Add files via upload

---
+++ Management/Database/Interface/ILogProcess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApplication.DataBase.Interface
+
+{
+    public interface ILogProcess
+    {
+        string LogIn(int userid, string password);
+        string LogOut();
+    }
+}
+
---
+++ Management/Database/Interface/ISignProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApplication.Enumerations;
+
+namespace TaskManagementApplication.DataBase.Interface
+{
+    public interface ISignProcess
+    {
+        string SignUp(string name, string email, Role role, string password);
+        string SignOut();
+
+    }
+}
