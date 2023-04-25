dbe931de1818495743d666739cc5210d4d4f13cf
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-25 16:17:39 +0530

    Add files via upload

--- Management/Model/User.cs
+++ Management/Model/User.cs
@@ -17,12 +17,15 @@ namespace TaskManagementApplication.Model
             Email = email;
             Role = role;
             AssignedProjects = new List<Project>();
+            AssignedTasks = new List<Tasks>();
         }
         public int UserId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public Role Role { get; set; }
         public ICollection<Project> AssignedProjects { get; set; }
+        public ICollection<Tasks> AssignedTasks { get; set; }
+
         public override string ToString()
         {
             return string.Format($"Name : {this.Name} \n UserId : {this.UserId} \n Email : {this.Email} \n Role : {this.Role}");
