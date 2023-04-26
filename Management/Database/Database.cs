e1e195d54ef371e9373c9367d39e1419f21540f1
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-26 18:02:07 +0530

    Add files via upload

--- Management/Database/Database.cs
+++ Management/Database/Database.cs
@@ -11,31 +11,36 @@ namespace TaskManagementApplication.DataBase
 {
     public class Database
     {
-        private static Dictionary<int, User>? _allUsers;
+        private static Dictionary<int, ApprovedUser>? _allUsers;
         private static Dictionary<int, string>? _userCredentials;
         private static Dictionary<int, Project>? _allProjects;
         private static Dictionary<int, Tasks>? _allTasks;
+        private static Dictionary<string, User>? _temporaryUsers;
+        private static Dictionary<string, string>? _temporaryUserCredentials;
+        private static ICollection<string>? _approvedUsers;
         private readonly string adminPassword = "Admin@123";//TODO
 
         private int currentUser;
         private Admin admin;
-        private bool adminLogin;
+        private readonly bool adminLogin;
+        private string currentTemporaryUser;
         public int CurrentUser { get { return currentUser; } private set { currentUser = value; } }
+        public string CurrentTemporaryUser { get { return currentTemporaryUser; } private set { currentTemporaryUser = value; } }
         public Admin Admin { get { return admin; } }
         public string AdminPassword { get { return adminPassword; } }
         public bool AdminLoginStatus { get; private set; }
         private Database()
         {
-            _allUsers = new Dictionary<int, User>
+            _allUsers = new Dictionary<int, ApprovedUser>
             {
-                {1,new User("Lavanya","lava@gmail.com",Role.EMPLOYEE) },
-                {2 ,new User("Prithivi","prithivi.8@gmail.com",Role.MANAGER) },
-                {3,new User("Deepika","deepi@gmail.com",Role.LEAD) }
+                {1,new ApprovedUser("Lavanya","lava@gmail.com",Role.EMPLOYEE,UserApprovalOptions.APPROVED) },
+                {2 ,new ApprovedUser("Prithivi","prithivi.8@gmail.com",Role.MANAGER, UserApprovalOptions.APPROVED) },
+                {3,new ApprovedUser("Deepika","deepi@gmail.com",Role.LEAD, UserApprovalOptions.APPROVED) }
             };
             _allProjects = new Dictionary<int, Project>
             {
-                {1,new Project("P1","UI",2,StatusType.OPEN,PriorityType.MEDIUM,new DateTime(20/05/2023),new DateTime(20/06/2023)) },
-                {2,new Project("P2","Backend",2,StatusType.OPEN, PriorityType.LOW,new DateTime(01/06/2023),new DateTime(01/07/2023)) }
+                {1,new Project("P1","UI",2,StatusType.OPEN,PriorityType.MEDIUM,new DateOnly(2023,06,05),new DateOnly(2023,07,05)) },
+                {2,new Project("P2","Backend",2,StatusType.OPEN, PriorityType.LOW,new DateOnly(2023,10,05),new DateOnly(2023,11,05)) }
             };
             _userCredentials = new Dictionary<int, string>
             {
@@ -45,9 +50,12 @@ namespace TaskManagementApplication.DataBase
             };
             _allTasks = new Dictionary<int, Tasks>
             {
-                {1,new Tasks("T1","Task 1",2,StatusType.OPEN,PriorityType.MEDIUM,new DateTime(20/06/2023),new DateTime(20/07/2023),2) }
+                {1,new Tasks("T1","Task 1",2,StatusType.OPEN,PriorityType.MEDIUM,new DateOnly(2023,08,05),new DateOnly(2023,09,05),2) }
             };
             admin = new Admin("Admin", "admin@gmail.com");
+            _temporaryUsers = new Dictionary<string, User>();
+            _temporaryUserCredentials = new Dictionary<string, string>();
+            _approvedUsers = new List<string>();
         }
 
         private static readonly Database instance = new();
@@ -124,18 +132,31 @@ namespace TaskManagementApplication.DataBase
             return Result.FAILURE;
         }
         //User section
-        public Result AddUser(User user, string password)
+        public Result AddUser(ApprovedUser user)
         {
-            if (_allUsers!.ContainsKey(user.UserId))
-                return Result.FAILURE;
-            _allUsers.Add(user.UserId, user);
+            _approvedUsers!.Add(user.Email);
+            _temporaryUsers!.Remove(user.Email);
+            string password = _temporaryUserCredentials![user.Email];
+            _temporaryUserCredentials!.Remove(user.Email);
+            _allUsers!.Add(user.UserId, user);
             _userCredentials!.Add(user.UserId, password);
             return Result.SUCCESS;
         }
-
-        public User GetUser(int userId)
+        public Result AddTemporaryUser(User user, string password)
+        {
+            _temporaryUsers!.Add(user.Email, user);
+            _temporaryUserCredentials!.Add(user.Email, password);
+            Notification notification = new("A new signup request from the mail id :" + user.Email);
+            admin.Notifications.Add(notification.Id,notification);
+            return Result.SUCCESS;
+        }
+        public ApprovedUser GetUser(int userId)
         {
             return _allUsers![userId];
+        }
+        public User GetUser(string email)
+        {
+            return _temporaryUsers![email];
         }
         public bool IsUserAvailable(int userId)
         {
@@ -143,7 +164,22 @@ namespace TaskManagementApplication.DataBase
                 return true;
             return false;
         }
-        public Result DeleteUser()
+        public bool IsUserAvailable(string email)
+        {
+            foreach(ApprovedUser u in _allUsers!.Values)
+            {
+                if(u.Email == email) 
+                    return true;
+            }
+            return false;
+        }
+        public bool IsTemporaryUserAvailable(string email)
+        {
+            if(_temporaryUsers!.ContainsKey(email))
+                return true;
+            else return false;
+        }
+        public Result DeleteApprovedUser()
         {
             if (CurrentUser != 0)
             {
@@ -159,7 +195,7 @@ namespace TaskManagementApplication.DataBase
             else return Result.FAILURE;
         }
 
-        public Result CheckUser(int userId, string password)//pakka
+        public Result CheckApprovedUser(int userId, string password)
         {
             if (_allUsers!.ContainsKey(userId))
             {
@@ -172,7 +208,7 @@ namespace TaskManagementApplication.DataBase
             }
             else return Result.FAILURE;
         }
-        public Result LogOutUser()
+        public Result LogOutApprovedUser()
         {
             if (CurrentUser != 0)
             {
@@ -184,7 +220,60 @@ namespace TaskManagementApplication.DataBase
                 return Result.FAILURE;
             }
         }
+
+        public Result CheckTemporaryUser(string email,string password)
+        {
+            if(_temporaryUsers!.ContainsKey(email))
+            {
+                if (_temporaryUserCredentials![email] == password)
+                {
+                   CurrentTemporaryUser = email;
+                    return Result.SUCCESS;
+                }
+                else return Result.FAILURE;
+            }
+            else if(_approvedUsers!.Contains(email))
+            {
+                CurrentTemporaryUser = email;
+                return Result.PARTIAL;
+            }
+            else return Result.FAILURE;
+        }
+        public Result LogOutTemporaryUser()
+        {
+            if (CurrentTemporaryUser != null)
+            {
+                CurrentTemporaryUser = null ?? "";
+                return Result.SUCCESS;
+            }
+            else
+            {
+                return Result.FAILURE;
+            }
+        }
         //Admin section
-        public Result 
+        public Result LogInAdmin(int userId,string password) 
+        {
+            if (Admin.Id == userId)
+            {
+                if (AdminPassword == password)
+                {
+                    AdminLoginStatus = true;
+                    return Result.SUCCESS;
+                }
+                else return Result.PARTIAL;
+            }
+            else return Result.FAILURE;
+        }
+
+        public Result LogOutAdmin()
+        {
+            if (AdminLoginStatus)
+            {
+                AdminLoginStatus = false;
+                return Result.SUCCESS;
+            }
+            else return Result.FAILURE;
+        }
     }
     }
--- Management/Database/DatabaseHandler.cs
+++ Management/Database/DatabaseHandler.cs
@@ -11,32 +11,21 @@ using TaskManagementApplication.DataBase.Interface;
 
 namespace TaskManagementApplication.Controller
 {
-    public class DatabaseHandler : ISignProcess, ILogProcess
+    public class DatabaseHandler : ILogProcess
     {
         readonly Database _database = Database.GetInstance();
-        public string SignUp(string name, string email, Role role, string password)
-        {
-            User newUser = new(name, email, role);
-            if (_database.AddUser(newUser, password) == Result.FAILURE)
-                return "User already exists";
-            //else if (database.AddUser(newUser, password) != Result.SUCCESS) 
-            //  return "Some error occured";
-            else return "SignedUp successfully \nYour userId is : " + newUser.UserId;
-        }
-
         public string LogInUser(int userid, string password)
         {
-            if (_database.CheckUser(userid, password) == Result.SUCCESS)
+            if (_database.CheckApprovedUser(userid, password) == Result.SUCCESS)
                 return "Login successful";
-            else if (_database.CheckUser(userid, password) == Result.PARTIAL)
+            else if (_database.CheckApprovedUser(userid, password) == Result.PARTIAL)
                 return "Password incorrect";
             else return "Invalid userid";
 
         }      
         public string LogOutUser()
         {
-            //Console.WriteLine(Database.CurrentUser);
-            Result result = _database.LogOutUser();
+            Result result = _database.LogOutApprovedUser();
             if (result == Result.SUCCESS)
                 return "LoggedOut successfully";
             else return "Logout unsuccessful";
@@ -44,13 +33,43 @@ namespace TaskManagementApplication.Controller
         
         public string SignOutUser()
         {
-            //Console.WriteLine(Database.CurrentUser);
-            Result result = _database.DeleteUser();
+            Result result = _database.DeleteApprovedUser();
             if (result == Result.SUCCESS)
                 return "Your account deleted successfully";
             else if (result == Result.PARTIAL)
                 return "Cannot delete account now as you have projects pending to do";
             else return "Login yourself to proceed further";
+        }
+        public string LogInAdmin(int userid, string password)
+        {
+            if (_database.LogInAdmin(userid, password) == Result.SUCCESS)
+                return "Login successful";
+            else if (_database.LogInAdmin(userid, password) == Result.PARTIAL)
+                return "Password incorrect";
+            else return "Invalid userid";
+        }
+
+        public string LogOutAdmin()
+        {
+            if (_database.LogOutAdmin() == Result.SUCCESS)
+                return "Logged out successfully";
+            else return "Login yourself to proceed further";
+        }
+
+        public string LogInNonApprovedUser(string email,string password)
+        {
+            if (_database.CheckTemporaryUser(email, password) == Result.SUCCESS)
+                return "Login successful";
+            else if (_database.CheckTemporaryUser(email, password) == Result.PARTIAL)
+                return $"Your request have been approved, try logging in with your userid and password";
+            else return "Profile not available or password incorrect";//TODO should define another case to check and seperate the return stmt                  
+        }
+        public string LogOutTemporaryUser() 
+        {
+            Result result = _database.LogOutTemporaryUser();
+            if (result == Result.SUCCESS)
+                return "LoggedOut successfully";
+            else return "Logout unsuccessful";
         }
     }
 }
