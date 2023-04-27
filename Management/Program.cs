a94c2b290e60bdf77b478f4df0e573b5c0eeaf58
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-04-27 17:52:08 +0530

    Add files via upload

--- Management/Program.cs
+++ Management/Program.cs
@@ -1,8 +1,11 @@
-﻿using ConsoleApp1.PresentationLayer;
+﻿using TaskManagementApplication.Presentation;
+using TaskManagementApplication.Utils;
+
 class Program
 {
     static void Main(string[] args)
     {
+        ColorCode.DefaultCode("Welcome");
         Start.Run();
     }
 }
