02578e88631f2894e7ba8398aa083d0479205799
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-05-02 17:51:55 +0530

    Add files via upload

--- Management/Program.cs
+++ Management/Program.cs
@@ -9,3 +9,8 @@ class Program
         Start.Run();
     }
 }
+
+//LOGIC
+//user accesses the application using their email id and password -> -> done
+//all internal works related to the user will happen on their userid -> db and controller -> done
+//but the info displayed to the user will happen on their names -> presentation -> done
