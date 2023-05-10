501e9a8598163a2440c3702728a5ac441d82e6dc
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-05-10 12:33:40 +0530

    Add files via upload

---
+++ Management/Controller/Interfaces/IActivityList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApplication.Model;
+
+namespace TaskManagementApplication.Controller.Interface
+{
+    public interface IActivityList
+    {
+        public Dictionary<int, Project> ProjectsList();
+        public Dictionary<int, Tasks> TasksList();
+        public Dictionary<int, SubTask> SubTasksList();
+        public Dictionary<int, SmallSubTask> SmallSubTasksList();
+        public Dictionary<int, Issue> IssuesList();
+        public Dictionary<int, User> UsersList();
+    }
+}
